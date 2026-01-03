a17a6d823814dabcca35fbc3074140cf453ebcef
stephanebtvr <81235987+stephanebtvr@users.noreply.github.com>
2026-01-03 20:32:38 +0100

    backend/feature-api-gateway-discovery-service

---
+++ backend/discovery-service/src/test/java/com/kanban/discovery_service/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kanban.discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;     
+
+
+@SpringBootTest(properties = {"spring.main.web-application-type=none"})
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
